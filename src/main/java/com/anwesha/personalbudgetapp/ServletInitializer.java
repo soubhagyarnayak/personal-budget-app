75aaad421e30502087aa04de569ba79c2d9703ae
Soubhagya Nayak <soubhagya.r.nayak@gmail.com>
2019-01-02 18:59:01 +0530

    adding auto-generated spring boot files

---
+++ src/main/java/com/anwesha/personalbudgetapp/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.anwesha.personalbudgetapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PersonalBudgetAppApplication.class);
+	}
+
+}
+
---
+++ src/test/java/com/anwesha/personalbudgetapp/PersonalBudgetAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.anwesha.personalbudgetapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalBudgetAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
